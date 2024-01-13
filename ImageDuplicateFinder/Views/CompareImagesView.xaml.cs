3a38f1d2678ea622d639241b4aed2835a2f26b75
Julius Kirsch <Julius@skirsch.de>
2024-01-13 21:32:01 +0100

    feat: added compareImageView

--- ImageDuplicateFinder/Views/CompareImagesView.xaml.cs
+++ ImageDuplicateFinder/Views/CompareImagesView.xaml.cs
@@ -1,15 +1,49 @@
+using ImageDuplicateFinder.Dialogs;
+using ImageDuplicateFinder.Models;
+using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 
 namespace ImageDuplicateFinder.Views
 { 
     public sealed partial class CompareImagesView : Page
     {
+        private List<DuplicateImageItem> duplicates;
+
         public CompareImagesView()
         {
             this.InitializeComponent();
         }
 
-        public GridView CompareImageGrid => compareImageGrid;
-        public GridView DuplicatesGridView => duplicatesGridView;
+        private void duplicatesGridView_ItemClick(object sender, ItemClickEventArgs e)
+        {
+            compareImageGrid.Items.Clear();
+            var clickedDuplicate = e.ClickedItem as DuplicateImageItem;
+            foreach(var item in clickedDuplicate.Path)
+            {
+                compareImageGrid.Items.Add(item);
+            }
+        }
+
+        private void DeleteSelected_Click(object sender, Microsoft.UI.Xaml.RoutedEventArgs e)
+        {
+            List<string> deleteItems = compareImageGrid.SelectedItems.Select(x=>x.ToString()).ToList();
+            foreach (string path in deleteItems)
+            {
+                compareImageGrid.Items.Remove(path);
+                duplicates[duplicatesGridView.SelectedIndex].Path.Remove(path);
+                File.Delete(path);
+            }
+        }
+
+        private async void FindDuplicates_Click(object sender, RoutedEventArgs e)
+        {
+            var res = await new FindDuplicatesDialog().ShowAsync(pathTextbox.Text);
+            duplicates = res.Where(x => x.Value.Path.Count > 1).Select(x => x.Value).ToList();
+
+            duplicatesGridView.ItemsSource = duplicates;
+        }
     }
 }
